c4beff90fd83eddbf897236d4326953c01e0e7bf
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-01-05 12:58:17 +0100

    chore: #5 Added relations for user entity

--- src/Yana.Domain/Entites/TileHasUser.cs
+++ src/Yana.Domain/Entites/TileHasUser.cs
@@ -2,6 +2,8 @@
 
 public class TileHasUser
 {
+    public DateTime EditedDate { get; set; }
+
     public virtual Tile Tile { get; set; } = null!;
     public virtual User User { get; set; } = null!;
 }
--- src/Yana.Persistence/Configurations/UserConfiguration.cs
+++ src/Yana.Persistence/Configurations/UserConfiguration.cs
@@ -12,5 +12,27 @@ public class UserConfiguration : IEntityTypeConfiguration<User>
 
         builder.Property(x => x.AuthProvider)
             .HasDefaultValue(AuthProvider.Google);
+
+        builder
+            .HasMany(x => x.Documents)
+            .WithMany(x => x.Users)
+            .UsingEntity<DocumentHasUser>()
+            .HasKey(x => new { x.Document, x.User });
+
+        builder
+            .HasMany(x => x.Tiles)
+            .WithMany(x => x.Users)
+            .UsingEntity<TileHasUser>()
+            .HasKey(x => new { x.Tile, x.User });
+
+        builder
+            .HasMany(x => x.Tags)
+            .WithOne(x => x.User)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasMany(x => x.Citations)
+            .WithOne(x => x.User)
+            .OnDelete(DeleteBehavior.Cascade);
     }
 }
