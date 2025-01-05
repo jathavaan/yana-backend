de18d8af5c4be0c541977ad6d842d709e4e5c9ab
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-01-05 13:17:01 +0100

    chore: #5 Completed Citation entity configuration

--- src/Yana.Persistence/Configurations/CitationConfiguration.cs
+++ src/Yana.Persistence/Configurations/CitationConfiguration.cs
@@ -1,6 +1,35 @@
 ﻿namespace Yana.Persistence.Configurations;
 
-public class CitationConfiguration
+public class CitationConfiguration : IEntityTypeConfiguration<Citation>
 {
-    
+    public void Configure(EntityTypeBuilder<Citation> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder.Property(x => x.PublicationYear)
+            .IsRequired();
+
+        builder.Property(x => x.Title)
+            .IsRequired();
+
+        builder.Property(x => x.SourceType)
+            .IsRequired();
+
+        builder.Property(x => x.RetrievedDate)
+            .IsRequired();
+
+        builder
+            .HasOne(x => x.User)
+            .WithMany(x => x.Citations)
+            .OnDelete(DeleteBehavior.Cascade);
+
+        builder
+            .HasOne(x => x.Document)
+            .WithMany(x => x.Citations)
+            .OnDelete(DeleteBehavior.SetNull);
+    }
 }
