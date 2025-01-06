e9b4915901f9e1f1e3d4d3835b6c3e84b5d79cf2
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-01-06 10:33:57 +0100

    chore: #5 Configured relations for tile

--- src/Yana.Domain/Entites/Tile.cs
+++ src/Yana.Domain/Entites/Tile.cs
@@ -3,11 +3,12 @@
 public class Tile
 {
     public int Id { get; set; }
-    public string Content { get; set; } = null!;
+    public string? Content { get; set; }
     public int XPosition { get; set; }
     public int YPosition { get; set; }
     public int Width { get; set; }
-    public int Height { get; set; }
+    public int? Height { get; set; }
+    public DateTime CreatedDate { get; set; } = DateTime.Now;
 
     public virtual Document Document { get; set; } = null!;
     public virtual ICollection<User> Users { get; set; } = new List<User>();
--- src/Yana.Persistence/Configurations/TileConfiguration.cs
+++ src/Yana.Persistence/Configurations/TileConfiguration.cs
@@ -1,5 +1,47 @@
 ﻿namespace Yana.Persistence.Configurations;
 
-public class TileConfiguration
+public class TileConfiguration : IEntityTypeConfiguration<Tile>
 {
+    public void Configure(EntityTypeBuilder<Tile> builder)
+    {
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .ValueGeneratedOnAdd()
+            .IsRequired();
+
+        builder.Property(x => x.Content)
+            .ValueGeneratedNever()
+            .HasColumnType("ntext");
+
+        builder.Property(x => x.XPosition)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.YPosition)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.Height)
+            .ValueGeneratedNever()
+            .IsRequired();
+
+        builder.Property(x => x.Width)
+            .ValueGeneratedNever();
+
+        builder.Property(x => x.CreatedDate)
+            .IsRequired();
+
+        builder
+            .HasOne(x => x.Document)
+            .WithMany(x => x.Tiles)
+            .OnDelete(DeleteBehavior.Cascade)
+            .IsRequired();
+
+        builder
+            .HasMany(x => x.Users)
+            .WithMany(x => x.Tiles)
+            .UsingEntity<TileHasUser>()
+            .HasKey(x => new { x.Tile, x.User });
+    }
 }
