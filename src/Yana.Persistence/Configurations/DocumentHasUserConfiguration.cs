e4ab67c2d2e72be999c4fd4a4bd9d9d63b93e387
Jathavaan Shankarr <jathavaan12@gmail.com>
2025-01-06 10:39:57 +0100

    chore: #5 Setup up configuration for many-to-many table between tile and document

---
+++ src/Yana.Persistence/Configurations/DocumentHasUserConfiguration.cs
@@ -0,0 +1,11 @@
+﻿namespace Yana.Persistence.Configurations;
+
+public class DocumentHasUserConfiguration : IEntityTypeConfiguration<DocumentHasUser>
+{
+    public void Configure(EntityTypeBuilder<DocumentHasUser> builder)
+    {
+        builder.Property(x => x.Role)
+            .IsRequired()
+            .ValueGeneratedNever();
+    }
+}
